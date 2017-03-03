b4f5ef04acd334522c283911df378dc64ea405d9
Tunis <wazalop@gmail.com>
2017-03-03 15:31:52 +0100

    ajout salon/ConnectionUser

---
+++ src/vce/salon/ConnectionUser.java
@@ -0,0 +1,157 @@
+package vce.salon;
+
+import vce.data.SessionUser;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class ConnectionUser implements Runnable {
+
+    private Socket socketUser;
+    private Salon salon;
+    private String commandeSend = "";
+    private SessionUser sessionSend = null;
+
+    //Construct
+    //----------------------------------
+    public ConnectionUser(Socket client, Salon salon) {
+        this.socketUser = client;
+        this.salon = salon;
+
+        //Initialisation du Out
+        System.out.println("Initialisation Out"); //TODO : A suppr
+        Thread tOut = new Thread(new Out());
+        tOut.start();
+
+        //Initialisation du In
+        System.out.println("Initialisation In"); //TODO : A suppr
+        Thread tIn = new Thread(new In());
+        tIn.start();
+    }
+
+    //Setter
+    //----------------------------------
+    public void setSalonMapSocket(SessionUser session) {
+        synchronized (salon.getMapSocket()) {
+            this.salon.setMapSocket(session.getPseudo(), this);
+        }
+    }
+
+    public SessionUser getSessionSend() {
+        synchronized (sessionSend) {
+            return this.sessionSend;
+        }
+    }
+
+    //Method
+    //----------------------------------
+    //Commande : CURRENT_USER / QUESTIONNAIRE / SESSION / CLOSE
+    public void send(String commande) {
+        this.commandeSend = commande;
+    }
+
+    public void send(String commande, SessionUser session) {
+        this.commandeSend = commande;
+        this.sessionSend = session;
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+
+    }
+
+    //Inner Class
+    //----------------------------------
+    //Flux de sortie pour un client
+    class Out implements Runnable {
+        ObjectOutputStream oos = null;
+
+        //Construct
+        //----------------------------------
+        public Out() {
+            try {
+                this.oos = new ObjectOutputStream(socketUser.getOutputStream());
+                this.oos.flush();
+            } catch (IOException e) {
+                System.err.println("Erreur de flux Out : " + e.getMessage());
+            }
+        }
+
+        @Override
+        public void run() {
+            while (!socketUser.isClosed()) {
+                try {
+                    //Selon la commande re�us on envoi l'objet correspondant, sinon on ne fait rien
+                    switch (commandeSend) {
+                        case "CURRENT_USER":
+                            oos.writeObject(salon.getCurrentUser());
+                            oos.flush();
+                            oos.reset();
+                            break;
+                        case "SESSION":
+                            oos.writeObject(getSessionSend());
+                            oos.flush();
+                            oos.reset();
+                            break;
+                        case "QUESTIONNAIRE":
+                            oos.writeObject(salon.getQuestionnaire());
+                            oos.flush();
+                            oos.reset();
+                            break;
+                        case "CLOSE":
+                            oos.close();
+                            oos = null;
+                            break;
+                        default:
+                            break;
+                    }
+                } catch (IOException e) {
+                    System.err.println("Erreur de flux Out Run : " + e.getMessage());
+                }
+            }
+        }
+    }
+
+    //Flux d'entr� pour un client
+    //Ne re�ois que des objets SessionUser => met � jour la list de SessionUser et la MapSocket.
+    class In implements Runnable {
+        ObjectInputStream ois = null;
+        SessionUser session = null;
+        boolean firstCo = true;
+
+        //Construct
+        //----------------------------------
+        public In() {
+
+        }
+
+        @Override
+        public void run() {
+            try {
+                ois = new ObjectInputStream(socketUser.getInputStream());
+            } catch (IOException e) {
+                System.err.println("Erreur de flux In : " + e.getMessage());
+            }
+
+            while (!socketUser.isClosed()) {
+                try {
+                    session = (SessionUser) ois.readObject();
+
+                    if (firstCo != false) {
+                        setSalonMapSocket(session);
+                        firstCo = false;
+                    }
+
+                    send("SESSION", session);
+                    salon.setSessionList(session);
+
+                } catch (IOException | ClassNotFoundException e) {
+                    System.err.println("Erreur de flux In Run : " + e.getMessage());
+                }
+            }
+        }
+    }
+}
---
+++ src/vce/salon/Salon.java
@@ -0,0 +1,171 @@
+package vce.salon;
+
+import vce.data.Questionnaire;
+import vce.data.SessionUser;
+import vce.data.User;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.*;
+
+//TODO heritage session
+public class Salon {
+    private SessionUser currentUser;
+    private Map<String, ConnectionUser> mapSocket = new TreeMap<String, ConnectionUser>();
+    private List<SessionUser> sessionList = new ArrayList<SessionUser>();
+    private int durationMax;
+    private Questionnaire questionnaire;
+
+    //TestUnit
+    private Thread t = null;
+
+    //Construct
+    //----------------------------------
+    public Salon(User user, int duration) {
+        this.durationMax = duration;
+
+        System.out.println("Initialisation ServerCo"); //TODO : A suppr
+        t = new Thread(new ServerCo(this));
+        t.start();
+    }
+
+    //Getter
+    //----------------------------------
+    public int getDurationMax() {
+        return this.durationMax;
+    }
+
+    public SessionUser getCurrentUser() {
+        return this.currentUser;
+    }
+
+    public Map<String, ConnectionUser> getMapSocket() {
+        return this.mapSocket;
+    }
+
+    public List<SessionUser> getSessionList() {
+        return this.sessionList;
+    }
+
+    //Setter
+    //----------------------------------
+    public synchronized void setSessionList(SessionUser session) {
+        String pseudo = "";
+        boolean modifier = false;
+        ListIterator<SessionUser> it = this.sessionList.listIterator();
+
+        //On v�rifie que l'objet n'existe pas d�j�
+        while (it.hasNext()) {
+            pseudo = it.next().getPseudo();
+
+            if (session.getPseudo().equals(pseudo)) {
+                it.set(session);
+                modifier = true;
+            }
+        }
+
+        if (modifier != true) {
+            this.sessionList.add(session);
+        }
+
+        //Envois la nouvel/maj SessionUser � tous les clients
+        sendAll("SESSION", session);
+    }
+
+    public Questionnaire getQuestionnaire() {
+        return this.questionnaire;
+    }
+
+    public Thread getT() {
+        return this.t;
+    }
+
+    public synchronized void setMapSocket(String key, ConnectionUser value) {
+        this.mapSocket.put(key, value);
+    }
+
+    //Method
+    //----------------------------------
+    //Demande la g�n�ration du questionnaire, l'envoie � tous les clients = D�but de la session
+    public void startQuestionnaire() {
+
+        this.questionnaire = new Questionnaire(1000); //TODO avec BDD
+        sendAll("QUESTIONNAIRE", null);
+
+        //TODO TRAITEMENT SESSION
+    }
+
+    private void sendAll(String commande, SessionUser session) {
+        synchronized (mapSocket) {
+            //On envoi pour chaque clients
+            if (session != null) {
+                mapSocket.forEach((key, value) -> {
+                    if (session.getPseudo() != key) {
+                        value.send(commande, session);
+                    }
+                });
+            } else {
+                mapSocket.forEach((key, value) -> {
+                    value.send(commande);
+                });
+            }
+        }
+    }
+
+    //Inner Class
+    //----------------------------------
+    //Cr�er les connexions avec les clients
+    class ServerCo implements Runnable {
+        private Salon salon;
+        private int port = 30000;
+        private ServerSocket server;
+        private boolean isRunning = true;
+
+        //Construct
+        //----------------------------------
+        public ServerCo(Salon salon) {
+            this.salon = salon;
+
+            while (this.port <= 65535) {
+                try {
+                    this.server = new ServerSocket(this.port, 100, InetAddress.getByName("127.0.0.1")); // TODO A modifier juste avec le port
+                    System.out.println("Lancement du server sur le port : " + this.port); //TODO : A suppr
+                    this.port = 65536;
+                } catch (UnknownHostException e) {
+                    System.err.println("H�te inconnu : " + e.getMessage());
+                } catch (IOException e) {
+                    //System.err.println("Erreur de flux : " + e.getMessage() + "\n port :" + this.port);
+                    this.port++;
+                }
+            }
+        }
+
+        //Method
+        //----------------------------------
+        // TODO : G�rer la boucle "infinie"
+        public void run() {
+            while (isRunning == true) {
+                try {
+                    //On attend une connexion d'un client
+                    Socket client = this.server.accept();
+                    System.out.println("Connexion Client !"); //TODO : A suppr
+                    //Ouverture d'un thread pour traiter le client, puis on attend de nouveau les connexion
+                    Thread t = new Thread(new ConnectionUser(client, this.salon));
+                    t.start();
+                } catch (IOException e) {
+                    System.err.println("Erreur de flux : " + e.getMessage());
+                }
+            }
+
+            try {
+                this.server.close();
+            } catch (IOException e) {
+                System.err.println("Impossible de fermer le server : " + e.getMessage());
+                this.server = null;
+            }
+        }
+    }
+}
