84b009760ed74ed1e88cdf41267def00838c88ba
Tunis <wazalop@gmail.com>
2017-03-06 16:04:49 +0100

    affiche liste

--- src/vce/ihm/controllers/salon/SalonCtrl.java
+++ src/vce/ihm/controllers/salon/SalonCtrl.java
@@ -2,7 +2,9 @@ package vce.ihm.controllers.salon;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.util.Callback;
 import vce.data.SessionUser;
 import vce.ihm.controllers.RootCtrl;
 
@@ -21,10 +23,52 @@ public class SalonCtrl {
 			portSalon.setText(String.valueOf(rootCtrl.getSession().getSocket().getPort()));
 			btnLaunch.setVisible(false);
             listSalon.setItems(rootCtrl.getSession().getSessionList());
+            listSalon.setCellFactory(new Callback<ListView<SessionUser>, ListCell<SessionUser>>() {
+
+                @Override
+                public ListCell<SessionUser> call(ListView<SessionUser> p) {
+
+                    ListCell<SessionUser> cell = new ListCell<SessionUser>() {
+
+                        @Override
+                        protected void updateItem(SessionUser t, boolean bln) {
+                            super.updateItem(t, bln);
+                            if (t != null) {
+                                setText(t.getPseudo());
+                            }
+                        }
+
+                    };
+
+                    return cell;
+                }
+            });
+
         } else {
 			ipSalon.setText(rootCtrl.getSalon().getHost());
 			portSalon.setText(String.valueOf(rootCtrl.getSalon().getPort()));
 			btnLaunch.setVisible(true);
-		}
+            listSalon.setItems(rootCtrl.getSalon().getSessionList());
+            listSalon.setCellFactory(new Callback<ListView<SessionUser>, ListCell<SessionUser>>() {
+
+                @Override
+                public ListCell<SessionUser> call(ListView<SessionUser> p) {
+
+                    ListCell<SessionUser> cell = new ListCell<SessionUser>() {
+
+                        @Override
+                        protected void updateItem(SessionUser t, boolean bln) {
+                            super.updateItem(t, bln);
+                            if (t != null) {
+                                setText(t.getPseudo());
+                            }
+                        }
+
+                    };
+
+                    return cell;
+                }
+            });
+        }
 	}
 }
