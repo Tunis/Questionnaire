5f323b1ac319cc2929e7b948581bbf3189557506
Anaralith <anaralith@gmail.com>
2017-03-14 11:53:21 +0100

    A Faire : Génération des dossier res et pdf + copy img

--- src/vce/vues/Start.java
+++ src/vce/vues/Start.java
@@ -14,6 +14,9 @@ public class Start extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
+		//TODO : New thread avec génération des dossier res et pdf + ajout des images depuis l'archive
+		
 		primaryStage.setTitle("VCE project");
 		primaryStage.setMinHeight(600);
 		primaryStage.setMinWidth(800);
